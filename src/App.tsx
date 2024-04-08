dcec4e54747ae80a7853bcd6946d26a574171a15
Adriano Miranda <adrjmiranda@gmail.com>
2024-04-08 09:36:40 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import { FunctionComponent, ReactNode } from 'react';
+
+import './sass/base.scss';
+
+const App: FunctionComponent = (): ReactNode => {
+	return (
+		<>
+			<h1>Fashion Store</h1>
+		</>
+	);
+};
+
+export default App;
